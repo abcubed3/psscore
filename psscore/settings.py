b5c5743ba3c2fda71a75a363ba906bf2898619f2
Abdul <abdul@abdul-Ubuntu.(none)>
2015-01-11 02:37:18 +0100

    added requirement

--- psscore/settings.py
+++ psscore/settings.py
@@ -57,8 +57,10 @@ WSGI_APPLICATION = 'psscore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'psscore_db',
+        'USER': 'root',
+        'PASSWORD': 'acubed',
     }
 }
 
